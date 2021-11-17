cb2a916b1c04f5aae8d7b602e7d1159fd866e029
khoir-roni <khoir.roni@gmail.com>
2021-11-17 11:12:24 +0700

    add Platform alternatife display

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:personal_expenses_app/widgets/chart.dart';
@@ -143,7 +145,7 @@ class _MyAppState extends State<MyApp> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text('Show Chart'),
-                    Switch(
+                    Switch.adaptive(
                         value: _showChart,
                         onChanged: (val) {
                           setState(() {
@@ -175,10 +177,12 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => _startAddNewTransaction(context),
-      ),
+      floatingActionButton: Platform.isIOS
+          ? Container()
+          : FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => _startAddNewTransaction(context),
+            ),
     );
   }
 }
